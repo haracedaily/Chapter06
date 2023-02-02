c2704d0dfd0d7785949e9c33c83f303fa7e37851
admin <admin@YD05-13>
2023-02-02 12:06:23 +0900

    Chapter06 use to import

---
+++ Chapter06/src/com/yedam/include/Application.java
@@ -0,0 +1,15 @@
+package com.yedam.include;
+
+import com.yedam.oop.*;
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		Book book01 = new Book();
+		Car car01 = new Car();
+		
+
+	}
+
+}
